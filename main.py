b671ee731e48014b388f81fe627d172dde7bd4dc
OdairDias <odair2d@hotmail.com>
2023-01-30 10:03:14 -0300

    novo bd
    Signed-off-by: OdairDias <odair2d@hotmail.com>

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-import pandas as pd
-import psycopg2
+import mysql.connector
 
-# lendo arquivo
-dados=pd.read_csv('dados_trasnf')
+# conectando com o banco
+#dados=pd.read_csv('dados_trasnf')
 # Connect to the database
-#conn = psycopg2.connect(
-    #dbname="projeto_analise_criminalidade",
-    #user="postgres",
-    #password="odairdias78",
-    #host="localhost",
-    #port="5432"
-#)
+banco = mysql.connector.connect(
+    host="localhost",
+    user='root',
+    password="",
+  
+)
 
-# Create a cursor object
-#cur = conn.cursor()
+print(banco)
+cursor = banco.cursor()
 
 # Execute a query
-#cur.execute("SELECT * FROM TB_crim_sp")
+cursor.execute("CREATE DATABASE banco_crim_sp")
+
 # Insert the data into the "crimedata" table
 #dados.to_sql(name='TB_crim_sp', con=conn, if_exists='replace', index=False)
 
@@ -24,5 +23,5 @@ dados=pd.read_csv('dados_trasnf')
 #conn.commit()
 
 # Close the cursor and connection
-cur.close()
-conn.close()
+#cur.close()
+#conn.close()
