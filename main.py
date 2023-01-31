81bec4ea5e8b923b1bee79a17e2cb3f4600d97a6
OdairDias <odair2d@hotmail.com>
2023-01-31 10:28:48 -0300

    ajustes
    Signed-off-by: OdairDias <odair2d@hotmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import mysql.connector
 
 # conectando com o banco
@@ -13,14 +14,18 @@ banco = mysql.connector.connect(
 print(banco)
 cursor = banco.cursor()
 
+#criar variaveis a serem inseridas
+comando_SQL='INSERT INTO banco_crim_sp (ANO_BO,NUM_BO,NUMERO_BOLETIM,DATAOCORRENCIA,HORAOCORRENCIA,PERIDOOCORRENCIA,DATACOMUNICACAO,BO_AUTORIA,FLAGRANTE,NUMERO_BOLETIM_PRINCIPAL,LOGRADOURO,NUMERO,BAIRRO,CIDADE,UF,LATITUDE,LONGITUDE,DESCRICAOLOCAL,EXAME,SOLUCAO,DELEGACIA_NOME,DELEGACIA_CIRCUNSCRICAO,ESPECIE,RUBRICA,DESDOBRAMENTO,STATUS,PLACA_VEICULO,UF_VEICULO,CIDADE_VEICULO,DESCR_COR_VEICULO,DESCR_MARCA_VEICULO,ANO_FABRICACAO,ANO_MODELO,DESCR_TIPO_VEICULO) VALUES($s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s,$s)'
+dados=pd.read_csv('dados_roubo_veiculos_novembro_2022_completo_01.csv')
+dados=dados
 # Execute a query
-cursor.execute("CREATE DATABASE banco_crim_sp")
+cursor.execute(comando_SQL,dados)
 
 # Insert the data into the "crimedata" table
 #dados.to_sql(name='TB_crim_sp', con=conn, if_exists='replace', index=False)
 
 # Commit the changes
-#conn.commit()
+banco.commit()
 
 # Close the cursor and connection
 #cur.close()
